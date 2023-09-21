f8dbdf7852e6d04464cecd5d890dfcb101684d5a
Mahidhar Kosigi <mahidharkosigi@gmail.com>
2023-09-21 10:19:02 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
-import Clarifai from 'clarifai'
+import Signin from './components/Signin/Signin';
+//import Clarifai from 'clarifai'
 
 import Navigation from './components/Nav/Navigation';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
@@ -11,9 +12,9 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import properti from './constants.';
 
-const app = new Clarifai.App({
-  apiKey: '3008a44dd4cf43158db4a56dce637db2'
- });
+// const app = new Clarifai.App({
+//   apiKey: '3008a44dd4cf43158db4a56dce637db2'
+//  });
 
 
 
